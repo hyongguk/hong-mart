ed31f8e0a975a4d42c9fb82a3f442766817090a0
Hyong <hyongguk1984@gmail.com>
2021-02-17 21:59:20 +0900

    created test futures

--- djangoApplication/settings.py
+++ djangoApplication/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'djangoApplication.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_app',
+        'USER': 'HYONGGUK',
+        'HOST': '127.0.0.1',
+        "PORT": '5432',
     }
 }
 
